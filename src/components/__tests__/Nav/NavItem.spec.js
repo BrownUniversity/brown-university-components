1a3297a17ca5222751110f2c4636f228b6dc3e52
Colin Cummings <colin_cummings@brown.edu>
2018-12-13 10:32:38 -0500

    add NavItem specs

---
+++ src/components/__tests__/Nav/NavItem.spec.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { render } from 'react-testing-library';
+
+import NavContext from '../../Nav/NavContext';
+import NavItem from '../../Nav/NavItem';
+
+const renderNavItem = ({
+  context = {},
+  props = {},
+  children = <span />
+} = {}) => {
+  const { mobile = false, sub = false } = context;
+  const rtlUtils = render(
+    <NavContext.Provider value={{ mobile, sub }}>
+      <ul>
+        <NavItem {...props}>{children}</NavItem>
+      </ul>
+    </NavContext.Provider>
+  );
+  const tree = rtlUtils.container.firstChild;
+
+  return {
+    navItem: tree.firstChild,
+    ...rtlUtils
+  };
+};
+
+describe('NavItem', () => {
+  describe('children', () => {
+    it('should render children', () => {
+      const children = <span data-testid="nav-item-children" />;
+      const { getByTestId } = renderNavItem({ children });
+
+      expect(getByTestId('nav-item-children')).toBeInTheDocument();
+    });
+  });
+
+  describe('style', () => {
+    it('should render nav item with default styles', () => {
+      const { navItem } = renderNavItem();
+
+      expect(navItem).toMatchInlineSnapshot(`
+.c0 {
+  display: list-item;
+  padding: 9px 15px;
+}
+
+<li
+  class="c0"
+>
+  <span />
+</li>
+`);
+    });
+
+    it('should render nav item with mobile styles when alternate is provided', () => {
+      const { navItem } = renderNavItem({ context: { mobile: true } });
+
+      expect(navItem).toHaveStyleRule('border-top', '0.5px solid #C8C8C880');
+      expect(navItem).toHaveStyleRule('padding', '1rem 0');
+    });
+
+    it('should render nav item with sub styles when alternate is provided', () => {
+      const { navItem } = renderNavItem({ context: { sub: true } });
+
+      expect(navItem).not.toHaveStyleRule('border-top');
+      expect(navItem).toHaveStyleRule('padding', '8px 0 8px 0');
+    });
+  });
+});
