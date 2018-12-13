285abd3687802ecd701b425425adaa9013dda305
Colin Cummings <colin_cummings@brown.edu>
2018-12-13 14:23:19 -0500

    add SiteNav specs

---
+++ src/components/__tests__/SiteNav.spec.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+import { triggerWindowResize } from 'window-test-utils';
+
+import SiteNav from '../SiteNav';
+import breakpoints from '../../constants/breakpoints';
+
+const renderSiteNav = ({
+  props = {},
+  children = (
+    <React.Fragment>
+      <SiteNav.Item>
+        <SiteNav.Link>Link</SiteNav.Link>
+      </SiteNav.Item>
+      <SiteNav.Item>
+        <SiteNav.Link active>Active Link</SiteNav.Link>
+      </SiteNav.Item>
+      <SiteNav.Item>
+        <SiteNav.Link disabled>Link</SiteNav.Link>
+      </SiteNav.Item>
+    </React.Fragment>
+  )
+} = {}) => {
+  const rtlUtils = render(<SiteNav {...props}>{children}</SiteNav>);
+
+  return {
+    siteNav: rtlUtils.container.firstChild,
+    ...rtlUtils
+  };
+};
+
+describe('SiteNav', () => {
+  describe('when window width is above mobileBreakpoint', () => {
+    describe('style', () => {
+      describe('when banner variant is not provided', () => {
+        it('should render default site nav', () => {
+          const { siteNav } = renderSiteNav();
+
+          expect(siteNav).toMatchSnapshot();
+        });
+      });
+
+      describe('when banner variant is provided', () => {
+        it('should render default site nav with banner positioning', () => {
+          const { siteNav } = renderSiteNav({ props: { banner: true } });
+
+          expect(siteNav).toMatchSnapshot();
+        });
+      });
+    });
+  });
+
+  describe('when window width is below mobileBreakpoint', () => {
+    beforeEach(() => {
+      triggerWindowResize({ width: breakpoints.md - 1 });
+    });
+
+    describe('style', () => {
+      describe('when banner variant is not provided', () => {
+        it('should render mobile site nav', () => {
+          const { siteNav } = renderSiteNav();
+
+          expect(siteNav).toMatchSnapshot();
+        });
+      });
+
+      describe('when banner variant is provided', () => {
+        it('should render mobile site nav with banner positioning', () => {
+          const { siteNav } = renderSiteNav({ props: { banner: true } });
+
+          expect(siteNav).toMatchSnapshot();
+        });
+      });
+    });
+
+    it('should toggle mobile site nav on mobile toggle button click', () => {
+      const { getByLabelText } = renderSiteNav();
+      const mobileToggleButton = getByLabelText('Toggle site navigation');
+
+      expect(mobileToggleButton).toHaveAttribute('aria-expanded', 'false');
+      fireEvent.click(mobileToggleButton);
+      expect(mobileToggleButton).toHaveAttribute('aria-expanded', 'true');
+      fireEvent.click(mobileToggleButton);
+      expect(mobileToggleButton).toHaveAttribute('aria-expanded', 'false');
+    });
+  });
+});
