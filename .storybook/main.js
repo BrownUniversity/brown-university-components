c779e395547d457ba675ec18604554483f3b4d4a
Jordan <andrew_jordan@brown.edu>
2020-11-06 12:54:13 -0500

    From config.js to main.js

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  stories: ["../stories/*.stories.@(js)"],
+  addons: [
+    "@storybook/addon-essentials",
+    "@storybook/addon-actions",
+    "@storybook/addon-knobs",
+    "@storybook/addon-a11y",
+  ],
+};
---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { create } from "@storybook/theming/create";
+import { version } from "../package.json";
+
+export default create({
+  brandTitle: `Brown University Components ${version}`,
+});
